b0b4a35bf942df485660f0fc1e40f38108ae8b0d
Yash Rao Patankar <75780922+YashRaoPatankar@users.noreply.github.com>
2025-07-13 09:12:24 +0530

    Add files via upload

---
+++ listing.py
@@ -0,0 +1,9 @@
+# basis of list
+
+l1 = [1, 2, 3, 4, 5,6, 7, 8, 9, 10]
+exL1 = l1[:5]
+revEX = exL1
+print('Original List:', l1)
+print('Extraxted first 5 elements:', exL1)
+revEX.reverse()
+print('Reversed extraxted List:', revEX)
